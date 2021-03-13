e1eccf893ec1eb3fe612830cf477329b9d546fc6
Shawn Santhoshgeorge <shawngeorge2001@gmail.com>
2021-03-13 12:52:33 -0500

    Update Header.js for Contact Page

--- src/components/Header.js
+++ src/components/Header.js
@@ -87,9 +87,9 @@ export default class Header extends Component {
                 <Scroll
                   onClick={_ => this.toggleMenu(!openMenu)}
                   type="id"
-                  element="signup"
+                  element="contact"
                 >
-                  <a className="nav-link" href="#signup">
+                  <a className="nav-link" href="#contact">
                     Contact
                   </a>
                 </Scroll>
