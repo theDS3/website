955f0fdf8ca6ae1ba5d30e47dac16ea1f6891db6
Shawn Santhoshgeorge <32755895+ShawnGeorge03@users.noreply.github.com>
2021-05-21 22:50:26 -0400

    Changed the team image paths

--- config.js
+++ config.js
@@ -10,6 +10,31 @@ module.exports = {
   pathPrefix: `/gatsby-starter-grayscale/`, // This path is subpath of your hosting https://domain/portfolio
   heading: 'Data Science & Statistics Society',
   subHeading: 'Full time Web Developer. Part time Open source contributor  ',
+  teamImages: {
+      p1: 'src/assets/images/teamImages/presidents/p1.JPG',
+      p2: 'src/assets/images/teamImages/presidents/p2.JPG',
+      p3: 'src/assets/images/teamImages/presidents/p3.JPG',
+      p4: 'src/assets/images/teamImages/presidents/p4.JPG',
+      p5: 'src/assets/images/teamImages/presidents/p5.JPG',
+      p6: 'src/assets/images/teamImages/presidents/p6.JPG',
+      p7: 'src/assets/images/teamImages/presidents/p7.JPG',
+
+      p8: 'src/assets/images/teamImages/directors/d1.jpg',
+      p9: 'src/assets/images/teamImages/directors/d2.jpg',
+      p10: 'src/assets/images/teamImages/directors/d3.jpg',
+      p11: 'src/assets/images/teamImages/directors/d4.jpg',
+
+      p12: 'src/assets/images/teamImages/social-design/s1.JPG',
+      p13: 'src/assets/images/teamImages/social-design/s2.JPG',
+
+      p14: 'src/assets/images/teamImages/associates/a1.jpg',
+      p15: 'src/assets/images/teamImages/associates/a2.jpg',
+      p16: 'src/assets/images/teamImages/associates/a3.jpg',
+      p17: 'src/assets/images/teamImages/associates/a4.jpg',
+      p18: 'src/assets/images/teamImages/associates/a5.jpg',
+      p19: 'src/assets/images/teamImages/associates/a6.jpg',
+      p20: 'src/assets/images/teamImages/associates/a7.jpg',
+    },
 
   // social
   socialLinks: [
