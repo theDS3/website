27dac1b7549902926cec7941b940033e245c88ca
Shawn Santhoshgeorge <shawngeorge2001@gmail.com>
2021-07-01 21:26:07 -0400

    Made the footer stick to the bottom of webpage

--- src/views/Footer.js
+++ src/views/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <footer className="bg-black small text-center text-white-50">
+    <footer className="fixed-bottom bg-black small text-center text-white-50">
       <div className="container">
         Copyright &copy; Data Science & Statistics Club 2021
       </div>
